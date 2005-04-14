382ecc813f60faad68ddcff1bc5932fe6d794986
CVS-to-SVN conversion program <cvs2svn@icecube.wisc.edu>
2005-04-14 12:01:01 -0500

    This commit was manufactured by cvs2svn to create tag 'V00-00-00'.

---
+++ src/domSControlRoutines.c
@@ -0,0 +1,12 @@
+
+#include "DOMtypes.h"
+#include "domSControlRoutines.h"
+
+USHORT readADCrept(UBYTE adc, UBYTE msecDelay, UBYTE *data) {
+    return 24;
+}
+
+BOOLEAN testDOMconstraints(ULONG constraint) {
+    return TRUE;
+}
+
